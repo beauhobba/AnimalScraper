cceef2453b9a5024c306dd3bbe4ba099bec87c0d
beauhobba <hobbabeau@gmail.com>
2022-08-02 09:07:16 +1000

    Added: Data for location and ecology

--- web_scraper.py
+++ web_scraper.py
@@ -5,11 +5,12 @@ import re
 
 f = open('./test.csv', 'w')
 writer = csv.writer(f, lineterminator = '\n')
-writer.writerow(["Animal Name", "Scientific Name", "Status"])
+writer.writerow(["Animal Name", "Scientific Name", "Status, Ibra, Eco Descriptors"])
 
 # List of monotremes and marsupials of Australia WebScraper 
 
 web_url = "https://en.wikipedia.org/wiki/List_of_monotremes_and_marsupials_of_Australia"
+gov_db = "https://biodiversity.org.au/afd/taxa/"
 
 response = requests.get(
 	url=web_url,
@@ -20,6 +21,52 @@ soup = BeautifulSoup(response.content, 'html.parser')
 title = soup.find(id="bodyContent").find_all("li")
 
 
+
+def get_eco_details(scientific_name):
+    IBRA = ""
+    eco_descriptors = "" 
+    try:
+        response = requests.get(
+            url=gov_db+scientific_name.replace(" ", "_"),
+            )
+        if(response.status_code != 200):
+            response = requests.get(
+                url=gov_db+scientific_name.replace(" ", "_")+";"+scientific_name.split(" ")[0]
+                )           
+            if(response.status_code != 200):
+                response = requests.get(
+                url=gov_db+scientific_name.replace(" ", "_").replace('(', '').split(')')[0]
+                )   
+                if(response.status_code != 200):
+                    raise Exception("No animal found")
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        ibra_flag = False 
+
+        for data in (soup.find(id='afdDistribution')): 
+            if(data.text == "IBRA"):
+                ibra_flag = True
+                continue
+            if(data.text != "" and data.text != None and ibra_flag == True ):
+                if((re.search(r"\bACT\b|\bNSW\b|\bQld\b|\bSA\b|\bTas\b|\bVic\b|\bWA\b|\bNT\b", str(data)) != None)):
+                    IBRA = (" ".join(str(data.text).split())) 
+                    break 
+        # backup checker
+        if(IBRA == ""):
+            for data in (soup.find(id='afdDistribution')): 
+                if((re.search(r"\bACT\b|\bNSW\b|\bQld\b|\bSA\b|\bTas\b|\bVic\b|\bWA\b|\bNT\b", str(data)) != None)):
+                    IBRA = (" ".join(str(data.text).split())) 
+                    break 
+                
+            
+        eco_descriptors = ((soup.find(id='afdEcologicalDescriptors').findAll('p'))[0]).text
+    except: 
+        pass  
+    
+    return IBRA, eco_descriptors
+        
+        
 
 for tag in title:
     name = ""
@@ -32,6 +79,11 @@ for tag in title:
             name = animal[1].split('>')[0]
             scientific_name = ((animal[1].split('<i>')[1])).split("<")[0]
             iucn_status = ((animal[2]).split(": ")[1]).split(">")[0]
-            writer.writerow([name, scientific_name, iucn_status])
+            IBRA = "" 
+            eco_descriptors = "" 
+            
+            IBRA, eco_descriptors = get_eco_details(scientific_name)
+            
+            writer.writerow([name, scientific_name, iucn_status, IBRA, eco_descriptors])
         except Exception as ex:
             print(ex)
